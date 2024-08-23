bd99914794f759a41c15fa57e8711c570df33d4c
Can AKYILDIRIM <akyildirimcan@gmail.com>
2024-08-23 09:37:57 +0300

    video-22

---
+++ javascript-refresher/start/assets/scripts/app.js
@@ -0,0 +1,69 @@
+// import { apiKey } from "./utils";
+// import apiKey from "./utils";
+//import { apiKey, abc } from "./utils";
+//import * as util from "./utils";
+//import { apiKey, abc as content } from "./utils";
+// console.log(util.apiKey); // abc123
+// console.log(util.abc); // abc
+//console.log(content); // abc
+
+// let userMessage = "Hello World!";
+
+// const userMessage2 = "Hello World!!";
+
+// console.log(userMessage2);
+
+// function greet() {
+//   console.log(userMessage);
+//   return userMessage;
+// }
+
+// greet();
+
+// export default (username, message) => {
+//   return username + message;
+// };
+
+// const username = "Max";
+
+// (userName) => {
+//   console.log(userName);
+// };
+
+// (number) => {
+//   return number * 3;
+// };
+
+// const username = "Max";
+// const age = 30;
+
+// const user = {
+//   name: "Max",
+//   age: 30,
+//   greet() {
+//     console.log("Hello");
+//     console.log(this.name);
+//   },
+// };
+
+// console.log(user.name);
+// user.greet();
+
+// class Person {
+//   name = "Max";
+
+//   constructor(age) {
+//     this.age = age;
+//   }
+
+//   greet() {
+//     console.log("Hello");
+//     console.log(this.name);
+//   }
+// }
+
+const hobbies = ["Sports", "Cooking"];
+
+for (let hobby of hobbies) {
+  console.log(hobby);
+}
---
+++ javascript-refresher/start/assets/scripts/utils.js
@@ -0,0 +1,5 @@
+// export let apiKey = "abc123";
+export default "abc123";
+
+export let apiKey = "abc123";
+export let abc = "abc";
